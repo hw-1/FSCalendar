49cc2fd6fb104583f680f6bfe662e66494bc3e5a
git <git@users.noreply.github.com>
2019-04-19 11:05:53 +0800

    FIX

--- FSCalendar/FSCalendarCell.m
+++ FSCalendar/FSCalendarCell.m
@@ -128,11 +128,12 @@
                                        );
     }
     
-    _imageView.frame = CGRectMake(self.preferredImageOffset.x, self.preferredImageOffset.y, self.contentView.fs_width, self.contentView.fs_height);
+    _imageView.frame = CGRectMake(self.preferredImageOffset.x, self.preferredImageOffset.y, self.contentView.fs_width, self.contentView.fs_height - 3);
     
     CGFloat titleHeight = self.bounds.size.height*5.0/6.0;
-    CGFloat diameter = MIN(self.bounds.size.height*5.0/6.0,self.bounds.size.width);
-    diameter = diameter > FSCalendarStandardCellDiameter ? (diameter - (diameter-FSCalendarStandardCellDiameter)*0.5) : diameter;
+//    CGFloat diameter = MIN(self.bounds.size.height*5.0/6.0,self.bounds.size.width);
+//    diameter = diameter > FSCalendarStandardCellDiameter ? (diameter - (diameter-FSCalendarStandardCellDiameter)*0.5) : diameter;
+    CGFloat diameter = self.borderRadius * 2;
     _shapeLayer.frame = CGRectMake((self.bounds.size.width-diameter)/2,
                                    (titleHeight-diameter)/2,
                                    diameter,
